13daa8b490b0ff473d9a626d2857b7b9211961ab
Krystyna Swider <krystynaswider@admins-MacBook-Pro-2.local>
2017-12-06 14:02:01 -0800

    Ninja services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './data.service';
 
 import { AppComponent } from './app.component';
+import { CountComponent } from './count/count.component';
+import { PlacesComponent } from './places/places.component';
+import { InfoComponent } from './info/info.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountComponent,
+    PlacesComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
